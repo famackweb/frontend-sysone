6ae606c66aea67ed0454a225ef9ed8dcf3d6f245
Fabrício Júnior <fabriciojunior77@outlook.com>
2024-01-25 17:51:24 -0300

    initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>SysOne</h1>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        danger: "#E25C5C",
+
+        gray: {
+          50: "#D9D9D9",
+          100: "#BABABA",
+          200: "#E5E5E5",
+          400: "#5E5E5E",
+          500: "#99A4AF",
+          600: "#808080",
+          700: "#333333",
+          800: "#242424",
+        },
+        cyan: {
+          50: "#E6F3F4",
+          600: "#00858F",
+        },
+        green: {
+          50: "#B5E6D1",
+          400: "#37E688",
+          500: "#33B333",
+          600: "#00A000",
+          700: "#1C7C1C",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
